5ebb729b5f3a9227f27d3861481037cd0fc74d79
Sam Schantz <samvschantz@gmail.com>
2019-01-19 08:46:28 -0800

    messages render dynamically now - not hardcoded

--- src/ChattyPresentation.jsx
+++ src/ChattyPresentation.jsx
@@ -10,11 +10,13 @@ export const ChattyPresentation = props => {
 
   const messages = (
     <main className="messages">
-      props.messages.map
-{/*      <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
-      </div>
+      props.messages.map((message, i) => (
+        <div className="message" key={i}>
+          <span className="message-username">{message.username}</span>
+          <span className="message-content">{message.content}</span>
+        </div>
+        ));
+{/*
       <div className="message system">
         Anonymous1 changed their name to nomnom.
       </div>*/}
